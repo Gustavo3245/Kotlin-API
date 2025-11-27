ef59286a35a3a9c98dd90ce80d4eeb6ec82d9d32
Gustavo3245 <Gustavomoreiradev3245@gmail.com>
2025-11-27 17:35:28 -0300

    Implemeting register, login and refresh functions for Authorization and Validation

---
+++ src/main/kotlin/com/example/spring_aplication/SpringAplicationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_aplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAplicationApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAplicationApplication>(*args)
+}
